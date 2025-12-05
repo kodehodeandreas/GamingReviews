727252c7eda17b57ddc5eb4ea5e170506ccdf3c5
Andreas Hestetun <kodehodeandreas@gmail.com>
2025-12-05 09:15:29 +0100

    path til gh-pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig(({ mode }) => {
   return {
-    base: mode === "production" ? "/GameReviews/" : "/",
+    base: mode === "production" ? "/GamingReviews/" : "/",
     plugins: [react()],
   };
 });
